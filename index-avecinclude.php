4f556f2911a475d74b783b246c26741228642085
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-03-24 17:28:02 +0100

    suite de la formation

---
+++ index-avecinclude.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon super site</title>
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    
+    <header>
+       
+    </header>
+    
+    <!-- Le menu -->
+    
+    <?php include("index-menu.php"); ?>
+    
+    <!-- Le corps -->
+    
+    <div id="corps">
+        <h1>Mon super site avec le menu en include</h1>
+        
+        <p>
+            Bienvenue sur mon super site !<br />
+            Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
+        </p>
+    </div>
+    
+    <!-- Le pied de page -->
+    
+    <footer id="pied_de_page">
+        <p>Copyright moi, tous droits réservés</p>
+    </footer>
+    
+    </body>
+</html>
--- variables.php
+++ variables.php
@@ -37,3 +37,61 @@ while ($nombre_de_lignes <= 10)
     $nombre_de_lignes++; // $nombre_de_lignes = $nombre_de_lignes + 1
 }
 ?>
+<?php
+// On crée notre array $prenoms
+$prenoms = array ('François', 'Michel', 'Nicole', 'Véronique', 'Benoît');
+
+// Puis on fait une boucle pour tout afficher :
+for ($numero = 0; $numero < 5; $numero++)
+{
+    echo $prenoms[$numero] . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
+}
+?>
+<?php
+$coordonnees = array (
+    'prenom' => 'François',
+    'nom' => 'Dupont',
+    'adresse' => '3 Rue du Paradis',
+    'ville' => 'Marseille');
+
+foreach($coordonnees as $element)
+{
+    echo $element . '<br />';
+}
+?>
+
+<?php
+$coordonnees = array (
+    'prenom' => 'François',
+    'nom' => 'Dupont',
+    'adresse' => '3 Rue du Paradis',
+    'ville' => 'Marseille');
+
+foreach($coordonnees as $cle => $element)
+{
+    echo '[' . $cle . '] vaut ' . $element . '<br />';
+}
+?>
+<?php
+$coordonnees = array (
+    'prenom' => 'François',
+    'nom' => 'Dupont',
+    'adresse' => '3 Rue du Paradis',
+    'ville' => 'Marseille');
+
+echo '<pre>';
+print_r($coordonnees);
+echo '</pre>';
+?>
+<?php
+$fruits = array ('Banane', 'Pomme', 'Poire', 'Cerise', 'Fraise', 'Framboise');
+
+$position = array_search('Fraise', $fruits);
+echo '"Fraise" se trouve en position ' . $position . '<br />';
+
+$position = array_search('Banane', $fruits);
+echo '"Banane" se trouve en position ' . $position . '<br />';
+
+$position = array_search('TOTO', $fruits);
+echo '"TOTO" se trouve en position ' . $position . '<br />';
+?>
