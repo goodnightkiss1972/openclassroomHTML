f56560c8120a940fca2ed74399f93e43ee83300d
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-04-11 17:18:44 +0200

    minichat

---
+++ minichat.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `test`.`minichat` 
+( 
+    `ID` INT NOT NULL AUTO_INCREMENT , 
+    `pseudo` VARCHAR(255) NOT NULL , 
+    `message` TEXT NOT NULL , PRIMARY KEY (`ID`)
+) 
+ENGINE = InnoDB;
+
---
+++ test-query-mysql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `test`.`news` 
+( 
+    `id` INT NOT NULL AUTO_INCREMENT , 
+    `titre` VARCHAR(255) NOT NULL , 
+    `contenu` TEXT NOT NULL , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
