cf3ff9ff73db465a10a3850888821aa8bde8ccfa
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-05-09 09:57:53 +0200

    retouche

--- espace_membre/menu.php
+++ espace_membre/menu.php
@@ -16,11 +16,8 @@ if (!isset($_SESSION['login'])) {
             echo "<li><a href=\"menu_profil.php\">Bienvenue chez Bubbles !</a></li>";
             echo "<li><a href=\"connexion_form.php\">S'inscrire / Se connecter</a></li>";
         } else {
-            echo "<li><a href=\"menu_profil.php\">Bienvenue ";
-            echo $_SESSION['login'];
-            echo " - Consulter votre profil";
-            echo "</a></li>";
-            echo "<li><a href=\"utilisateur_liste.php\">Gerer utilisateurs</a></li>";
+            echo "<li><a href=\"menu_profil.php\">Bienvenue ".$_SESSION['login']."</a></li>";
+            echo "<li><a href=\"utilisateur_liste.php\">Gerer les amis</a></li>";
             echo "<li><a href=\"deconnexion.php\">Se deconnecter</a></li>";
         }
         ?>
