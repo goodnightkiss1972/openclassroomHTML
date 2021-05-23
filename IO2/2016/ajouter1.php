bc1c1200697c9d115cc663b8693874a3989f8953
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-05-23 16:33:01 +0200

    gestion du cas sans produit coché

--- IO2/2016/ajouter1.php
+++ IO2/2016/ajouter1.php
@@ -1,9 +1,14 @@
 <?php
 
-foreach ($_POST['idArticle'] as $article) {
-    echo $article."<br/>";
+if (isset($_POST['TabidArticles']) && !empty($_POST['TabidArticles'])) {
+    echo "Produits ajoutés aux favoris<br/><br/>";
+
+    foreach ($_POST['TabidArticles'] as $article) {
+        echo $article."<br/>";
+    }
 }
-
-
+else {
+    echo "Aucun produit n'a été ajouté.";
+}
 
 ?>
